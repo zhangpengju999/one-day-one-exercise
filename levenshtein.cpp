195134e4a47cfb487230b0fff136545b76c51099
Jiaquan Fang <jiaquanfang@creditease.cn>
2018-02-24 01:12:06 +0800

    update

--- levenshtein.cpp
+++ levenshtein.cpp
@@ -6,41 +6,39 @@
 
 using namespace std;
 
-#define LOC(row, col, stride) (row) * (stride) + (col)
+size_t LevenshteinDistance(const string &s1, const string &s2) {
+    size_t l1 = s1.size() + 1;
+    size_t l2 = s2.size() + 1;
 
-size_t levenshtein_distance(const string &s1, const string &s2) {
-    size_t len_s1 = s1.size();
-    size_t len_s2 = s2.size();
-
-    size_t stride = len_s1 + 1;
-    vector<size_t> m(stride * (len_s2 + 1));
-
-    for (size_t i = 0; i < len_s1 + 1; ++i) 
+    unique_ptr<size_t[]> m(new size_t[l1 * l2]);
+    for (size_t i = 0; i < l1; ++i)
         m[i] = i;
-    for (size_t j = 1; j < len_s2 + 1; ++j)
-        m[LOC(j, 0, stride)] = j;
-    for (size_t i = 1; i < len_s1 + 1; ++i) { 
-        for (size_t j = 1; j < len_s2 + 1; ++j) {
-            size_t d1 = m[LOC(j - 1, i - 1, stride)];
-            size_t d2 = m[LOC(j - 1, i, stride)];
-            size_t d3 = m[LOC(j, i - 1, stride)];
-            size_t d = min(min(d1, d2), d3);
-            if (s1[i - 1] == s2[j - 1]) 
-                m[LOC(j, i, stride)] = d;
-            else 
-                m[LOC(j, i, stride)] = d + 1;
+    for (size_t j = 1; j < l2; ++j)
+        m[j * l1] = j;
+
+    for (size_t j = 1; j < l2; ++j) {
+        for (size_t i = 1; i < l1; ++i) {
+            size_t d1 = m[(j - 1) * l1 + i - 1];
+            size_t d2 = m[(j - 1) * l1 + i];
+            size_t d3 = m[j * l1 + i - 1];
+            if (s1[i - 1] == s2[j - 1])
+                m[j * l1 + i] = min(min(d2 + 1, d3 + 1), d1);
+            else
+                m[j * l1 + i] = min(min(d2 + 1, d3 + 1), d1 + 1);
         }
     }
 
-    return m[LOC(len_s2, len_s1, stride)];
+    return m[l1 * l2 - 1];
 }
 
 int main(void) {
 
-    cout << levenshtein_distance("", "ab") << endl;
-    cout << levenshtein_distance("abc", "cba") << endl;
-    cout << levenshtein_distance("string", "story") << endl;
-    cout << levenshtein_distance("book", "block") << endl;
+    cout << LevenshteinDistance("", "ab") << endl;
+    cout << LevenshteinDistance("abc", "cba") << endl;
+    cout << LevenshteinDistance("string", "story") << endl;
+    cout << LevenshteinDistance("book", "block") << endl;
+    cout << LevenshteinDistance("pneumonoultramicroscopicsilicovolcanoconiosis",
+                                "ultramicroscopically") << endl;
 
     return 0;
 }
